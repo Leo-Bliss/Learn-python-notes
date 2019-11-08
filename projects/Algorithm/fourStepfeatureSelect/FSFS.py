0a61e2f70e29293ed085f1a79986cc27913b5690
tbyouth <2638961251@qq.com>
2019-11-08 11:50:17 +0800

    处理数据方式修正

--- projects/Algorithm/fourStepfeatureSelect/FSFS.py
+++ projects/Algorithm/fourStepfeatureSelect/FSFS.py
@@ -18,29 +18,30 @@ Voting
 from sklearn.model_selection import KFold
 import numpy as np
 import pandas as pd
-import xlrd
+import math
+# import xlrd
 from sklearn.cross_decomposition import PLSRegression
 
 class FSFS:
-    def __init__(self):
+    def __init__(self,path):
         self.n_components = 3
-        self.pls = PLSRegression(n_components=self.n_components)
+        self.pls = PLSRegression(n_components=self.n_components,scale=True)
+        self.path = path
         pass
 
-    #划分数据：划分为k份
-    def split_data(self,k=10):
-        path = r'C:\Users\Administrator\AppData\Local\Programs\Python\Python37\Learn-python-notes\material\科研实践\数据\data1.XlsX'
-        df = pd.read_excel(path,sheet_name='Sheet1')
+    def run(self,k=10):
+        df = pd.read_excel(self.path,sheet_name='Sheet1',index_col=0)
+
         #print(df.columns.values)
         #第一行，第一列
-        head_list = list(df.columns.values)
+        header_list = list(df.columns.values)
         index_list = list(df.index.values)
-        # print(head_list)
+        # print(header_list)
         # print(indeX_list)
         #自变量X，因变量y
-        X = df[head_list[:-1]] #<class 'pandas.core.frame.DataFrame'>
+        X = df[header_list[:-1]] #<class 'pandas.core.frame.DataFrame'>
         # print(X.shape)
-        y = df[head_list[-1]] #<class 'pandas.core.series.Series'>
+        y = df[header_list[-1]] #<class 'pandas.core.series.Series'>
         # print(y.shape)
         # print(y.name)
         #将数据分为k份，k折交叉验证
@@ -50,33 +51,47 @@ class FSFS:
             # print('+-------------------+')
             # print(test_index)
             X_train,y_train = X.values[train_index],y[train_index]
-            #转换为DataFrame格式便于计算
-            X_train = pd.DataFrame(X_train,index=train_index,columns=head_list[:-1])
-            y_train = pd.DataFrame(y_train,index=train_index)
-            print(X_train)
-            print('+----------------------------------------------------------+')
-            print(y_train)
 
+            #转换为DataFrame格式便于计算
+            X_train = pd.DataFrame(X_train,index=train_index,columns=header_list[:-1])
+            y_train = pd.DataFrame(y_train.values,index=train_index,columns=[header_list[-1]])
             X_test,y_test = X.values[test_index],y[test_index]
-            X_test = pd.DataFrame(X_test,index=test_index,columns=head_list[:-1])
-            y_test = pd.DataFrame(y_test,index=test_index)
-            # print(X_test)
+            X_test = pd.DataFrame(X_test,index=test_index,columns=header_list[:-1])
+            y_test = pd.DataFrame(y_test.values,index=test_index,columns=[header_list[-1]])
+
+            #训练得到相关参数，即得到拟合方程
+            self.pls.fit(X_train, y_train)
+            #代入测试集中的自变量得到预测的因变量
+            y_test_predict = self.pls.predict(X_test)
+
+            # print(y_test_predict)
+            # print(type(y_test_predict))
+            # print(y_test)
+            # print(type(y_test.values))
+
+            #得到未开始特征选择的RMSE值
+            old_RMSE = self.get_RMSE(y_test_predict,y_test)
+            print(old_RMSE)
+
+            # Filter
+
             break
+
+    #RMSE
+    def get_RMSE(self,y_predict,y_test):
+        MSE = np.mean((y_test - y_predict) ** 2)
+        RMSE = math.sqrt(MSE)
+        return RMSE
 
     #PLS
     def PLS(self):
-        # self.pls.fit()
         pass
 
 
 
 
-
-
-
-
 if __name__ =='__main__':
-   f = FSFS()
-   f.split_data()
-
+   path = r'C:\Users\Administrator\AppData\Local\Programs\Python\Python37\Learn-python-notes\material\科研实践\数据\data1.xlsx'
+   f = FSFS(path)
+   f.run()
 
