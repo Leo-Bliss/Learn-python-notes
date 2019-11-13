3e9e10b4af3d3f9a38378a56f3c83df191607a01
tbyouth <2638961251@qq.com>
2019-11-13 19:36:50 +0800

    QlistWidget中放checkBox功能完善

--- projects/demo/Table/ListWidgetInsertCheckBoxDemo.py
+++ projects/demo/Table/ListWidgetInsertCheckBoxDemo.py
@@ -14,7 +14,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget,QCheckBox,QListWidget,QVBoxLayout
 from PyQt5.QtWidgets import QLineEdit,QPushButton,QHBoxLayout,QListWidgetItem,QFileDialog
-import xlrd
+import pandas as pd
 
 class Demo(QWidget):
     def __init__(self):
@@ -37,7 +37,7 @@ class Demo(QWidget):
         hlayout = QHBoxLayout()
         hlayout.addWidget(self.button1)
         hlayout.addWidget(self.button2)
-        self.list_widget = QListWidget()
+        self.list_widget1 = QListWidget()
         '''
         self.check_box1 = QCheckBox('x1')
         self.check_box2 = QCheckBox('x2')
@@ -48,73 +48,118 @@ class Demo(QWidget):
         item2 = QListWidgetItem()
         item3 = QListWidgetItem()
 
-        self.list_widget.addItem(item1)
-        self.list_widget.addItem(item2)
-        self.list_widget.addItem(item3)
-        self.list_widget.setItemWidget(item1,self.check_box1)
-        self.list_widget.setItemWidget(item2,self.check_box2)
-        self.list_widget.setItemWidget(item3,self.check_box3)
+        self.list_widget1.addItem(item1)
+        self.list_widget1.addItem(item2)
+        self.list_widget1.addItem(item3)
+        self.list_widget1.setItemWidget(item1,self.check_box1)
+        self.list_widget1.setItemWidget(item2,self.check_box2)
+        self.list_widget1.setItemWidget(item3,self.check_box3)
         '''
-
+        self.list_widget2 = QListWidget()
+        self.button3 = QPushButton('<=>')
+        hlayout2 = QHBoxLayout()
+        hlayout2.addWidget(self.list_widget1)
+        hlayout2.addWidget(self.button3)
+        hlayout2.addWidget(self.list_widget2)
         vlayout = QVBoxLayout()
         vlayout.addItem(hlayout1)
         vlayout.addItem(hlayout)
-        vlayout.addWidget(self.list_widget)
+        vlayout.addItem(hlayout2)
         self.setLayout(vlayout)
 
         self.open_button.clicked.connect(self.open)
-        self.button1.clicked.connect(self.selectAll)
-        self.button2.clicked.connect(self.cancelSelectAll)
+        self.button1.clicked.connect(self.selectAllStatus)
+        self.button2.clicked.connect(self.selectAllStatus)
+        self.button3.clicked.connect(self.add)
+
 
     def open(self):
-        file_name,_=QFileDialog.getOpenFileName(self,'打开EXCEL','E:/','EXcel文件(*.xlsx *.xls *.csv)')
+        file_dir = 'E:/' if self.line_edit1.text()=='' else self.line_edit1.text()
+        file_name,_=QFileDialog.getOpenFileName(self,'打开EXCEL',file_dir,'Excel文件(*.xlsx *.xls *.csv)')
         print(file_name)
-        #xlsx
-        if file_name.find('.xlsx'):
-            header_list = open_xlsx(file_name)
-            self.cnt = len(header_list)
-            for i in range(self.cnt):
+        if file_name == '':
+            return
+        self.line_edit1.setText(file_name)
+        header_list = open_excel(file_name) if file_name.find('.xls')!=-1 else open_csv(file_name)
+        self.cnt = len(header_list)
+        for i in range(self.cnt):
+            item = QListWidgetItem()
+            # print('&&&', item)
+            check_box = QCheckBox(header_list[i])
+            check_box.setChecked(True)
+            # print('@@@', check_box)
+            self.list_widget1.addItem(item)
+            self.list_widget1.setItemWidget(item, check_box)
+            '''
+            此处涉及到，到底是创建准确对象(有命名)还是临时对象的问题，
+            创建准确对象，方便访问，但是有一个如何不断创建准确对象问题，
+            创建临时对象，存在需要时该如何访问的问题。
+            前者暂时没有解决，我采用后者
+            '''
+            # 访问之前创建的临时对象
+            now_item = self.list_widget1.item(i).listWidget().item(i)
+            # print(now_item)
+            now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
+            now_check_box.clicked.connect(self.getCheckBoxStatus)
+            # print(now_check_box)
+
+    def getCheckBoxStatus(self):
+        for i in range(self.list_widget1.count()):
+            now_item = self.list_widget1.item(i).listWidget().item(i)
+            now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
+            print(now_check_box.text(),now_check_box.isChecked())
+        print('-------------------------')
+
+    def selectAllStatus(self):
+        # self.cnt = self.list_widget1.count()
+        # print(self.cnt)
+        # for i in range(1,self.cnt+1):
+        #     item = 'self.check_box%s.setChecked(True)'%str(i)
+        #     eval(item)
+        sender = self.sender()
+        print(sender.text())
+        status = sender.text() == '确认全选'
+        for i in range(self.list_widget1.count()):
+            now_item = self.list_widget1.item(i).listWidget().item(i)
+            now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
+            now_check_box.setChecked(status)
+
+    def add(self):
+        for i in range(self.list_widget1.count()):
+            now_item = self.list_widget1.item(i).listWidget().item(i)
+            now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
+            if(now_check_box.isChecked()):
                 item = QListWidgetItem()
-                check_box = QCheckBox(header_list[i])
+                check_box = QCheckBox(now_check_box.text())
                 check_box.setChecked(True)
-                self.list_widget.addItem(item)
-                self.list_widget.setItemWidget(item,check_box)
-                '''
-                此处涉及到，到底是创建准确对象(有命名)还是临时对象的问题，
-                创建准确对象，方便访问，但是有一个如何不断创建准确对象问题，
-                创建临时对象，存在需要时该如何访问的问题
-                '''
-                #checkState(),True:2,False:0
-                self.list_widget.item(i).listWidget().item(0).setCheckState()
-                print(self.list_widget.item(i).listWidget().item(0).checkState())
+                self.list_widget2.addItem(item)
+                self.list_widget2.setItemWidget(item, check_box)
+                now_check_box.setChecked(False)
+                now_check_box.setEnabled(False)
+                # print(self.list_widget1.item(i))
+                # self.list_widget1.removeItemWidget(self.list_widget1.item(i))
 
-        elif file_name.find('.xls'):
-            pass
-        else:
-            pass
+# def open_xlsx(file_name):
+#     import xlrd
+#     wb = xlrd.open_workbook(file_name)
+#     sheet1 = wb.sheet_by_index(0)
+#     header_labels = sheet1.row_values(0)
+#     print(header_labels)
+#     return header_labels
 
+def open_csv(file_name):
+    df = pd.read_csv(file_name)
+    head_list = df.columns.values
+    # print(head_list)
+    # print(df.index.values)
+    return list(head_list)
 
-    def selectAll(self):
-        self.cnt = self.list_widget.count()
-        print(self.cnt)
-        for i in range(1,self.cnt+1):
-            item = 'self.check_box%s.setChecked(True)'%str(i)
-            eval(item)
+def open_excel(file_name):
+    df = pd.read_excel(file_name,sheet_name='Sheet1',index_col=0)
+    head_list = df.columns.values
+    # print(head_list)
+    return list(head_list)
 
-    def cancelSelectAll(self):
-        self.cnt = self.list_widget.count()
-        print(self.cnt)
-        for i in range(1, self.cnt + 1):
-            item = 'self.check_box%s.setChecked(False)' % str(i)
-            eval(item)
-
-
-def open_xlsx(file_name):
-    wb = xlrd.open_workbook(file_name)
-    sheet1 = wb.sheet_by_index(0)
-    header_labels = sheet1.row_values(0)
-    print(header_labels)
-    return header_labels
 
 
 if __name__=='__main__':
