5ac520546d4ea8c763172b79237e797cae5872f8
tbyouth <2638961251@qq.com>
2019-12-08 22:33:20 +0800

    文件名调整

--- projects/CTF/gif_dump/gifToPictures.py
+++ projects/CTF/gif_dump/gifToPictures.py
@@ -12,6 +12,7 @@ gif图片分离成图片
 
 from PIL import Image
 import os
+# from projects.CTF.QR_code import QRCode
 
 def gif_to_pictures(gif_file):
   image = Image.open(gif_file)
@@ -29,3 +30,10 @@ def gif_to_pictures(gif_file):
 if __name__=='__main__':
   gif_file = r'./masterGo.gif'
   gif_to_pictures(gif_file)
+  # qr = QRCode.QRCodeDemo()
+  # res = ''
+  # for i in range(18):
+  #   file = './masterGo/{0}.png'.format(i)
+  #   image = Image.open(file)
+  #   res += qr.scan(image)
+  # print(res)
