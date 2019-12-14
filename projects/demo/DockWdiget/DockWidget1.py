1a021ac97941b43387a1c7f19ff3318c83ed86f9
tbyouth <2638961251@qq.com>
2019-12-15 00:13:03 +0800

    悬浮窗Demo

--- projects/demo/DockWdiget/DockWidget1.py
+++ projects/demo/DockWdiget/DockWidget1.py
@@ -20,15 +20,31 @@ class DockWidgetDemo(QMainWindow):
 
         layout= QHBoxLayout()
         self.items = QDockWidget('DockWidget',self)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.items.setSizePolicy(sizePolicy)
+        self.items.setFixedHeight(80)
         self.listWidget = QListWidget()
         for i in range(5):
             self.listWidget.addItem('item%s'%i)
         self.items.setWidget(self.listWidget)
-        self.setCentralWidget(QLineEdit())
+        '''
+            在QMainWindow中加入QWidget可以突破QMainWindow中使用其他布局无效的局限,
+            同理，在各种窗口中加入其他窗口，可以将窗口展现的更丰富
+        '''
+        gridLayout = QGridLayout(self)
+
+        widget = QWidget()
+        layout = QHBoxLayout()
+        layout.addWidget(QTextEdit())
+        gridLayout.addItem(layout)
+        widget.setLayout(gridLayout)
+
+        self.setCentralWidget(widget)
+        # self.setCentralWidget(QLineEdit())
         #设置默认悬浮
         # self.items.setFloating((True))
 
-        self.addDockWidget(Qt.RightDockWidgetArea,self.items)
+        self.addDockWidget(Qt.TopDockWidgetArea,self.items)
 
 
 if __name__ == '__main__':
