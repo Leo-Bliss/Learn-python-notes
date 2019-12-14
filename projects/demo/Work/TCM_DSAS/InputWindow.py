3a2c3dc861e753d9c3debfaa2e4cdc96c60fca40
tbyouth <2638961251@qq.com>
2019-12-14 19:57:23 +0800

    上线查找并标记功能

--- projects/demo/Work/TCM_DSAS/InputWindow.py
+++ projects/demo/Work/TCM_DSAS/InputWindow.py
@@ -14,14 +14,78 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget,QTabWidget
-from PyQt5.QtWidgets import QTableView,QFileDialog
-from PyQt5.QtWidgets import QMenuBar,QToolBar,QStatusBar,QAction
-from PyQt5.QtWidgets import QVBoxLayout,QHeaderView
-from PyQt5.QtGui import QStandardItemModel,QPixmap,QIcon,QStandardItem
+from PyQt5.QtWidgets import QTableView,QFileDialog,QPushButton
+from PyQt5.QtWidgets import QMenuBar,QToolBar,QStatusBar,QAction,QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout,QHeaderView,QSizePolicy,QLineEdit
+from PyQt5.QtGui import QStandardItemModel,QPixmap,QIcon,QStandardItem,QColor
 from PyQt5.QtCore import Qt,QDir
 from openpyxl import workbook
 import xlrd
 import time
+
+#查找替换窗口
+class  FindWidget(QWidget):
+    def __init__(self):
+        super(FindWidget,self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(80,800)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Fixed)
+
+        self.line_edit1 = QLineEdit()
+        self.line_edit1.setPlaceholderText('find')
+        self.line_edit2 = QLineEdit()
+        self.line_edit2.setPlaceholderText('replace')
+
+        self.tool_bar = QToolBar()
+        self.search = QAction('查找')
+        self.up = QAction('向上')
+        self.down = QAction('向下')
+        self.close = QAction('关闭')
+        self.tool_bar.addAction(self.search)
+        self.tool_bar.addAction(self.down)
+        self.tool_bar.addAction(self.up)
+        self.tool_bar.addAction(self.close)
+
+
+
+        self.repalce_button = QPushButton('Replace')
+        self.repalceAll_button = QPushButton('ReplaceAll')
+
+
+        vlayout1 = QVBoxLayout()
+        vlayout1.addWidget(self.line_edit1)
+        vlayout1.addWidget(self.line_edit2)
+
+        vlayout2 = QVBoxLayout()
+        vlayout2.addWidget(self.tool_bar)
+
+        hlayout1 = QHBoxLayout()
+        hlayout1.addWidget(self.repalce_button)
+        hlayout1.addWidget(self.repalceAll_button)
+        vlayout2.addItem(hlayout1)
+
+        layout = QHBoxLayout()
+        layout.addItem(vlayout1)
+        layout.addItem(vlayout2)
+        self.setLayout(layout)
+
+        self.close.triggered.connect(self.triggeredClose)
+        icon = QIcon()
+        icon.addPixmap(QPixmap('./image/查找.png'), QIcon.Normal, QIcon.Off)
+        self.search.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/向下.png'), QIcon.Normal, QIcon.Off)
+        self.down.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/向上.png'), QIcon.Normal, QIcon.Off)
+        self.up.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/关闭.png'), QIcon.Normal, QIcon.Off)
+        self.close.setIcon(icon)
+
+    def triggeredClose(self):
+        self.hide()
+
 
 
 class WidgetDemo(QWidget):
@@ -34,6 +98,8 @@ class WidgetDemo(QWidget):
         self.resize(800,800)
         #这里初始化，便于直接输入数据
         self.data = [['']*100 for i in range(15000)]
+        self.res_pos = []
+        self.focus_pos = None
         #菜单栏
         self.menu = QMenuBar()
         self.file = self.menu.addMenu('文件')
@@ -47,10 +113,7 @@ class WidgetDemo(QWidget):
         self.open = self.file.addAction('打开')
         self.save = self.file.addAction('保存')
         # self.save_as = self.file.addAction('另存为')
-        #快捷键
-        self.open.setShortcut('Ctrl+O')
-        self.save.setShortcut('Ctrl+S')
-        self.new.setShortcut('Ctrl+N')
+
 
         #编辑菜单下的子菜单
         self.cut = self.edit.addAction('剪切')
@@ -58,8 +121,13 @@ class WidgetDemo(QWidget):
         self.paste = self.edit.addAction('粘贴')
         self.delete = self.edit.addAction('删除')
         self.find = self.edit.addAction('查找')
-        # self.replace = self.find.addAction('替换')
         self.replace = self.edit.addAction('替换')
+
+        # 快捷键
+        self.open.setShortcut('Ctrl+O')
+        self.save.setShortcut('Ctrl+S')
+        self.new.setShortcut('Ctrl+N')
+        self.find.setShortcut('Ctrl+F')
 
         #视图菜单下的子菜单
         self.tool_view = QAction('工具栏',checkable=True)
@@ -83,11 +151,14 @@ class WidgetDemo(QWidget):
         self.tool_bar.addAction(self.copy)
         self.tool_bar.addAction(self.paste)
         self.tool_bar.addAction(self.find)
-        self.tool_bar.addAction(self.replace)
+        # self.tool_bar.addAction(self.replace)
 
-        #tool文本显示在下方
-        self.tool_bar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        # #tool文本显示在下方
+        # self.tool_bar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
+        #findWidge
+        self.find_widget = FindWidget()
+        self.find_widget.hide()
 
         #表格
         self.table_view = QTableView()
@@ -102,6 +173,7 @@ class WidgetDemo(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.menu)
         layout.addWidget(self.tool_bar)
+        layout.addWidget(self.find_widget)
         layout.addWidget(self.table_view)
         layout.addWidget(self.status_bar)
         self.setLayout(layout)
@@ -113,16 +185,34 @@ class WidgetDemo(QWidget):
         self.tool_view.triggered.connect(self.triggeredView)
         self.statu_view.triggered.connect(self.triggeredView)
         self.new.triggered.connect(self.triggeredNew)
+        self.find.triggered.connect(self.triggeredFind)
+        self.find_widget.search.triggered.connect(self.dataLocation)
+        self.find_widget.down.triggered.connect(self.downLocation)
+        self.find_widget.up.triggered.connect(self.upLocation)
+        self.find_widget.close.triggered.connect(self.triggeredHideFind)
+        self.find_widget.repalce_button.clicked.connect(self.onClickReplace)
+        self.find_widget.repalceAll_button.clicked.connect(self.onClickReplaceAll)
 
         #美化
         icon = QIcon()
         icon.addPixmap(QPixmap('./image/打开.png'), QIcon.Normal, QIcon.Off)
         self.open.setIcon(icon)
-        # icon = QIcon()
         icon.addPixmap(QPixmap('./image/保存.png'), QIcon.Normal, QIcon.Off)
         self.save.setIcon(icon)
         icon.addPixmap(QPixmap('./image/新建.png'), QIcon.Normal, QIcon.Off)
         self.new.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/剪切.png'), QIcon.Normal, QIcon.Off)
+        self.cut.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/复制.png'), QIcon.Normal, QIcon.Off)
+        self.copy.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/粘贴.png'), QIcon.Normal, QIcon.Off)
+        self.paste.setIcon(icon)
+        icon.addPixmap(QPixmap('./image/查找1.png'), QIcon.Normal, QIcon.Off)
+        self.find.setIcon(icon)
+        # icon.addPixmap(QPixmap('./image/替换.png'), QIcon.Normal, QIcon.Off)
+        # self.replace.setIcon(icon)
+
+
 
     def triggeredOpen(self):
         self.status_bar.showMessage('打开文件',5000)
@@ -166,14 +256,16 @@ class WidgetDemo(QWidget):
             wa = wb.active
             for item in self.data:
                 # 过滤无效数据
-                if ''.join(item) == '':
-                    continue
+                try:
+                    if ''.join(item) == '':
+                        continue
+                except:
+                    pass
                 wa.append(item)
             wb.save(file_path)
             self.status_bar.showMessage('保存完毕！')
         except Exception as e:
             print(e)
-
 
 
     #数据变化信号处理
@@ -202,6 +294,105 @@ class WidgetDemo(QWidget):
     def triggeredNew(self):
         print('New')
         pass
+
+    def triggeredFind(self):
+        self.find_widget.show()
+
+    #重载信号，实现ESC隐藏查找窗口
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.find_widget.hide()
+
+    #聚焦到某个cell
+    def positionFocus(self,x,y):
+        self.table_view.verticalScrollBar().setSliderPosition(x)
+        self.table_view.horizontalScrollBar().setSliderPosition(y)
+        self.table_view.openPersistentEditor(self.mode.index(x, y))
+        self.table_view.setFocus()
+
+    #得到所以匹配项的位置
+    def dataLocation(self):
+        text = self.find_widget.line_edit1.text()
+        self.res_pos = []
+        flag = 0
+        for i,row in enumerate(self.data):
+            try:
+                if ''.join(row) == '':
+                    continue
+            except:
+                pass
+            for j,cell in enumerate(row):
+                if text == str(cell):
+                    # print(i,j)
+                    self.res_pos.append((i,j))
+                    item = self.mode.item(i,j)
+                    item.setBackground(QColor(255, 255, 0))
+                    item.setForeground(QColor(255, 0, 0))
+                    #转到到第一个匹配值的位置，并处于可编辑状态
+                    if not flag:
+                        flag = 1
+                        self.positionFocus(i,j)
+                        self.focus_pos = 0
+
+    #向下跳转
+    def downLocation(self):
+        cnt = len(self.res_pos)
+        if cnt == 0 or self.focus_pos == cnt-1:
+            return
+        try:
+            self.table_view.closePersistentEditor(
+                self.mode.index(self.res_pos[self.focus_pos][0],self.res_pos[self.focus_pos][1]))
+            x, y = self.res_pos[self.focus_pos + 1]
+            self.positionFocus(x,y)
+            self.focus_pos += 1
+        except Exception as e:
+            print(e)
+
+    # 向上跳转
+    def upLocation(self):
+        cnt = len(self.res_pos)
+        if cnt == 0 or self.focus_pos == 0:
+            return
+        try:
+            self.table_view.closePersistentEditor(
+                self.mode.index(self.res_pos[self.focus_pos][0], self.res_pos[self.focus_pos][1]))
+            x, y = self.res_pos[self.focus_pos - 1]
+            self.positionFocus(x, y)
+            self.focus_pos -= 1
+        except Exception as e:
+            print(e)
+
+
+    def triggeredHideFind(self):
+        if self.res_pos is not None and len(self.res_pos):
+            self.table_view.closePersistentEditor(
+                self.mode.index(self.res_pos[self.focus_pos][0], self.res_pos[self.focus_pos][1]))
+            for item in self.res_pos:
+                x,y = item
+                item = self.mode.item(x,y)
+                item.setBackground(QColor(255, 255, 255))
+                item.setForeground(QColor(0, 0, 0))
+        self.find_widget.hide()
+
+
+    #不清楚如何修改cell值，替换功能暂时无法上线
+    def onClickReplace(self):
+        print('-'*50)
+        cnt = len(self.res_pos)
+        if self.res_pos is None or cnt == 0:
+            return
+        try:
+
+            x, y = self.res_pos[self.focus_pos]
+            text = self.find_widget.line_edit2.text()
+            self.data[x][y] = text
+            print(self.data[x][y])
+        except Exception as e:
+            print(e)
+
+    def onClickReplaceAll(self):
+        pass
+
 
 
 
