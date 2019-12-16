015faaec3d75ce9a935459d45e5480069c30d5be
tbyouth <2638961251@qq.com>
2019-12-16 14:34:05 +0800

    修正选中添加

--- projects/demo/Table/ListWidgetInsertCheckBoxDemo.py
+++ projects/demo/Table/ListWidgetInsertCheckBoxDemo.py
@@ -12,9 +12,8 @@
 3.打开excel，将表格水平标签设置为check_box的文本内容
 '''
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QCheckBox,QListWidget,QVBoxLayout
+from PyQt5.QtWidgets import QApplication,QWidget,QCheckBox,QListWidget,QVBoxLayout,QAbstractItemView
 from PyQt5.QtWidgets import QLineEdit,QPushButton,QHBoxLayout,QListWidgetItem,QFileDialog
-import pandas as pd
 
 class Demo(QWidget):
     def __init__(self):
@@ -56,6 +55,7 @@ class Demo(QWidget):
         self.list_widget1.setItemWidget(item3,self.check_box3)
         '''
         self.list_widget2 = QListWidget()
+        self.list_widget1.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.button3 = QPushButton('<=>')
         hlayout2 = QHBoxLayout()
         hlayout2.addWidget(self.list_widget1)
@@ -80,7 +80,7 @@ class Demo(QWidget):
         if file_name == '':
             return
         self.line_edit1.setText(file_name)
-        header_list = open_excel(file_name) if file_name.find('.xls')!=-1 else open_csv(file_name)
+        header_list = read_xlsx(file_name)[0]
         self.cnt = len(header_list)
         for i in range(self.cnt):
             item = QListWidgetItem()
@@ -101,21 +101,14 @@ class Demo(QWidget):
             # print(now_item)
             now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
             now_check_box.clicked.connect(self.getCheckBoxStatus)
-            # print(now_check_box)
+
 
     def getCheckBoxStatus(self):
         for i in range(self.list_widget1.count()):
             now_item = self.list_widget1.item(i).listWidget().item(i)
             now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
-            print(now_check_box.text(),now_check_box.isChecked())
-        print('-------------------------')
 
     def selectAllStatus(self):
-        # self.cnt = self.list_widget1.count()
-        # print(self.cnt)
-        # for i in range(1,self.cnt+1):
-        #     item = 'self.check_box%s.setChecked(True)'%str(i)
-        #     eval(item)
         sender = self.sender()
         print(sender.text())
         status = sender.text() == '确认全选'
@@ -124,45 +117,44 @@ class Demo(QWidget):
             now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
             now_check_box.setChecked(status)
 
+    def innnerSelectAll(self):
+        #全选
+        self.list_widget1.selectAll()
+
+        # #取消全选
+        # self.list_widget1.clearSelection()
+
+        # # 将选中项移除
+        # for selectedItem in self.list_widget1.selectedItems():
+        #     self.list_widget1.takeItem(self.list_widget1.row(selectedItem))
+
     def add(self):
-        for i in range(self.list_widget1.count()):
-
-            now_item = self.list_widget1.item(i).listWidget().item(i)
-            now_check_box = self.list_widget1.item(i).listWidget().itemWidget(now_item)
+        count = self.list_widget1.count()
+        index = 0
+        while index < count:
+            now_item = self.list_widget1.item(index).listWidget().item(index)
+            now_check_box = self.list_widget1.item(index).listWidget().itemWidget(now_item)
             if(now_check_box.isChecked()):
-                    print(self.list_widget1.takeItem(i))
+                item = QListWidgetItem()
+                check_box = QCheckBox(now_check_box.text())
+                check_box.setChecked(True)
+                self.list_widget2.addItem(item)
+                self.list_widget2.setItemWidget(item, check_box)
+                self.list_widget1.takeItem(index)
+                count -= 1
+            else:
+                index += 1
 
-            #     # item = QListWidgetItem()
-            #     # check_box = QCheckBox(now_check_box.text())
-            #     # check_box.setChecked(True)
-            #     # self.list_widget2.addItem(item)
-            #     # self.list_widget2.setItemWidget(item, check_box)
-            #     # now_check_box.setChecked(False)
-            #     # now_check_box.setEnabled(False)
-            #     self.list_widget1.takeItem(i)
-            #     # print(self.list_widget1.item(i))
-            #     # self.list_widget1.removeItemWidget(self.list_widget1.item(i))
 
-# def open_xlsx(file_name):
-#     import xlrd
-#     wb = xlrd.open_workbook(file_name)
-#     sheet1 = wb.sheet_by_index(0)
-#     header_labels = sheet1.row_values(0)
-#     print(header_labels)
-#     return header_labels
 
-def open_csv(file_name):
-    df = pd.read_csv(file_name)
-    head_list = df.columns.values
-    # print(head_list)
-    # print(df.index.values)
-    return list(head_list)
-
-def open_excel(file_name):
-    df = pd.read_excel(file_name,sheet_name='Sheet1',index_col=0)
-    head_list = df.columns.values
-    # print(head_list)
-    return list(head_list)
+#xls,xlxs,csv
+def read_xlsx(path):
+    import xlrd
+    workbook = xlrd.open_workbook(path)
+    sheet1 = workbook.sheet_by_index(0)
+    rows = len(sheet1.col_values(0))
+    data = [sheet1.row_values(i) for i in range(rows)]
+    return data
 
 
 
